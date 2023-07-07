81e3de1ff1a1c01c2f63391be2c7b0d17d3db665
luxiopppp <lucio.ese@gmail.com>
2023-07-07 10:29:43 -0300

    gitignore, readme y eslint config

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,27 +1,24 @@
-import { useState } from "react"
-import confetti from "canvas-confetti"
+import { useState } from 'react'
+import confetti from 'canvas-confetti'
 
-import { Square } from "./components/Square.jsx"
-import { TURNS } from "./constants.js"
-import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
-import { WinnerModal } from "./components/WinnerModal.jsx"
-import { saveGameToStorage, resetGameStorage } from "./logic/storage/index.js"
+import { Square } from './components/Square.jsx'
+import { TURNS } from './constants.js'
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
+import { WinnerModal } from './components/WinnerModal.jsx'
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js'
 
-function App() {
-
+function App () {
   const [board, setBoard] = useState(() => {
     const boardFromStorage = window.localStorage.getItem('board')
     return boardFromStorage
-    ? JSON.parse(boardFromStorage)
-    : Array(9).fill(null)
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null)
   })
-  const [turn, setTurn] = useState(() =>{
+  const [turn, setTurn] = useState(() => {
     const turnFromStorage = window.localStorage.getItem('turn')
     return turnFromStorage ?? TURNS.X
   })
-  const [winner, setWinner] = useState(null) //null no hay ganador, false hay empate
-
-  
+  const [winner, setWinner] = useState(null) // null no hay ganador, false hay empate
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
@@ -56,40 +53,30 @@ function App() {
       setWinner(newWinner)
       confetti()
     } else if (checkEndGame(newBoard)) {
-      setWinner(false)  
+      setWinner(false)
     }
   }
-  
+
   return (
-    <main className="board">
+    <main className='board'>
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reset Game</button>
-      <section className="game">
-        {
-          board.map((square, index) => {
-            return (
-              <Square
-                key={index}
-                index={index}
-                updateBoard={updateBoard}
-              >
-                {square}
-              </Square>
-            )
-          })
-        }
+      <section className='game'>
+        {board.map((square, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {square}
+            </Square>
+          )
+        })}
       </section>
 
-      <section className="turn">
-        <Square isSelected={turn === TURNS.X}>
-          {TURNS.X}
-          </Square>
-        <Square isSelected={turn === TURNS.O}>
-          {TURNS.O}
-        </Square>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
-      
-      <WinnerModal resetGame={resetGame} winner={winner}/>
+
+      <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
   )
 }
